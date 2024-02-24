382ffb19bf3d98068792acee23b01978fba261fc
control-alt-f4 <109923311+CardboardDog@users.noreply.github.com>
2024-02-24 14:52:33 -0500

    minimal file reading
    - settings can be read from .eyn files
    - levels can be read from .eyn files

--- src/Main.c
+++ src/Main.c
@@ -4,15 +4,23 @@
 #include "SRCE/loader.h"
 #include "SRCE/graphics.h"
 int main(int argc, char* args[]){
-    load_settings("sss");
+    int settings[2];
+    load_settings(&settings[0],&settings[1]);
+    int** level;
+    level = malloc(settings[0] * sizeof(*level));
+    for (int i = 0; i < settings[1]; i++) {
+        level[i] = malloc(settings[0] * sizeof(level[0]));
+    }
     if(SDL_Init(SDL_INIT_VIDEO)<0){
         printf("Could not init SDL2: \n\t%s", SDL_GetError());
     }
     init_display();
     clear();
+    printf("x6y4:%d",load_level_block("DEMO",6,4));
     while(!should_exit()){
         line(0,0,300,200,3);
         update();
     }
     quit();
+    return 0;
 }
--- src/SRCE/loader.h
+++ src/SRCE/loader.h
@@ -2,11 +2,59 @@
 #include <stdbool.h>
 #include <stdio.h>
 FILE* gameFile;
-void load_settings(char levelName[]){
+int load_level_block(char* level, int x, int y){
     gameFile = fopen(FILE_PATH,"r");
     char charBuffer[68];
+    int settingsOpen = 0;
+    int scanY = 0;
+    char beginLevelName[68] = "NAME ";
+    strcat(beginLevelName, level); // NAME [level]
     while(fscanf(gameFile,"%68[^\n]\n",charBuffer)==1){
-        printf("%s[\\N]\n",charBuffer);
+        if(strcmp(charBuffer,"LBFER:")==0){
+            settingsOpen = 1;
+        }
+        if(settingsOpen==1 && strcmp(charBuffer,beginLevelName)==0){
+            settingsOpen = 2;
+        }
+        if(settingsOpen==2 && strcmp(charBuffer,"LWALL:")==0){
+            settingsOpen = 3;
+            scanY = 0;
+        }else if(settingsOpen==3){
+            if(strcmp(charBuffer,"END")==0){
+                break;
+            }
+            if(scanY==y){
+                fclose(gameFile);
+                char hexRetrieve[1];
+                hexRetrieve[0] = charBuffer[x];
+                return (int)strtol(hexRetrieve, NULL, 16);
+            }scanY++;
+        }
+    }
+    fclose(gameFile);
+    return 0;
+}
+void load_settings(int* gameWidth, int* gameHeight){
+    gameFile = fopen(FILE_PATH,"r");
+    char charBuffer[68];
+    int settingsOpen = 0;
+    while(fscanf(gameFile,"%68[^\n]\n",charBuffer)==1){
+        if(strcmp(charBuffer,"SMODE:")==0){
+            settingsOpen = 1;
+            printf("opening settings\n");
+        }
+        if(settingsOpen == 1){
+            if(strcmp(charBuffer,"END")==0){
+                settingsOpen = 0;
+                printf("closing settings\n");
+            }else if(strstr(charBuffer, "LWDTH")){
+                strtok(charBuffer," ");
+                *gameWidth = atoi(strtok(NULL,""));
+            }else if(strstr(charBuffer, "LHGHT")){
+                strtok(charBuffer," ");
+                *gameHeight = atoi(strtok(NULL,""));
+            }
+        }
     }
     fclose(gameFile);
 }

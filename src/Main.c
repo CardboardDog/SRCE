fdd7efae49ad2fc900539f8c36c982e4289fa998
control-alt-f4 <109923311+CardboardDog@users.noreply.github.com>
2024-02-26 15:11:40 -0500

    texture and start position loading
    - added start position loading
    - added texture loading

--- src/Main.c
+++ src/Main.c
@@ -6,17 +6,33 @@
 int main(int argc, char* args[]){
     int settings[2];
     load_settings(&settings[0],&settings[1]);
+    int playerX = 0, playerY = 0;
     int** level;
     level = malloc(settings[0] * sizeof(*level));
     for (int i = 0; i < settings[1]; i++) {
         level[i] = malloc(settings[0] * sizeof(level[0]));
     }
+    for (int y = 0; y < settings[1]; y++){
+        for (int x = 0; x < settings[0]; x++){
+            level[x][y] = load_level_block("DEMO",x,y);
+            printf("[%d]",load_level_block("DEMO",x,y));
+        }
+        printf("\n");
+    }
+    load_level_start("DEMO",&playerX,&playerY);
+    for (int y = 0; y < 64; y++){
+        for (int x = 0; x < 64; x++){
+            printf("[%d]",load_texture_pixel("WTYPE1",x,y));
+        }
+        printf("\n");
+    }
+    printf("PX:[%d]\n",playerX);
+    printf("PY:[%d]\n",playerY);
     if(SDL_Init(SDL_INIT_VIDEO)<0){
         printf("Could not init SDL2: \n\t%s", SDL_GetError());
     }
     init_display();
     clear();
-    printf("x6y4:%d",load_level_block("DEMO",6,4));
     while(!should_exit()){
         line(0,0,300,200,3);
         update();
--- src/SRCE/loader.h
+++ src/SRCE/loader.h
@@ -34,6 +34,34 @@ int load_level_block(char* level, int x, int y){
     fclose(gameFile);
     return 0;
 }
+void load_level_start(char* level, int* x, int* y){
+    gameFile = fopen(FILE_PATH,"r");
+    char charBuffer[68];
+    int settingsOpen = 0;
+    int scanY = 0;
+    char beginLevelName[68] = "NAME ";
+    strcat(beginLevelName, level); // NAME [level]
+    while(fscanf(gameFile,"%68[^\n]\n",charBuffer)==1){
+        if(strcmp(charBuffer,"LBFER:")==0){
+            settingsOpen = 1;
+        }
+        if(settingsOpen==1 && strcmp(charBuffer,beginLevelName)==0){
+            settingsOpen = 2;
+        }
+        if(settingsOpen==2){
+            if(strcmp(charBuffer,"END")==0){
+                break;
+            }if(strstr(charBuffer, "XSTRT")){
+                strtok(charBuffer," ");
+                *x = atoi(strtok(NULL,""));
+            }else if(strstr(charBuffer, "YSTRT")){
+                strtok(charBuffer," ");
+                *y = atoi(strtok(NULL,""));
+            }
+        }
+    }
+    fclose(gameFile);
+}
 void load_settings(int* gameWidth, int* gameHeight){
     gameFile = fopen(FILE_PATH,"r");
     char charBuffer[68];
@@ -57,4 +85,36 @@ void load_settings(int* gameWidth, int* gameHeight){
         }
     }
     fclose(gameFile);
+}
+int load_texture_pixel(char* level, int x, int y){
+    gameFile = fopen(FILE_PATH,"r");
+    char charBuffer[68];
+    int settingsOpen = 0;
+    int scanY = 0;
+    char beginLevelName[68] = "NAME ";
+    strcat(beginLevelName, level); // NAME [level]
+    while(fscanf(gameFile,"%68[^\n]\n",charBuffer)==1){
+        if(strcmp(charBuffer,"GMTEX:")==0){
+            settingsOpen = 1;
+        }
+        if(settingsOpen==1 && strcmp(charBuffer,beginLevelName)==0){
+            settingsOpen = 2;
+        }
+        if(settingsOpen==2 && strcmp(charBuffer,"TEXBF:")==0){
+            settingsOpen = 3;
+            scanY = 0;
+        }else if(settingsOpen==3){
+            if(strcmp(charBuffer,"END")==0){
+                break;
+            }
+            if(scanY==y){
+                fclose(gameFile);
+                char hexRetrieve[1];
+                hexRetrieve[0] = charBuffer[x];
+                return (int)strtol(hexRetrieve, NULL, 16);
+            }scanY++;
+        }
+    }
+    fclose(gameFile);
+    return 0;
 }
